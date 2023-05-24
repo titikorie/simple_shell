29257cb5caa462aea54bdd3a72af323e94dc49bc
amel83 <tesfaneshk222@gmail.com>
2023-05-24 11:55:20 -0700

    c

--- work.c
+++ work.c
@@ -30,20 +30,16 @@ int main(int numm, char **argv)
 			}
 		}
 		if (empty_line)
-		{
 			continue;
-		}
+
 		len = _strlen(line);
 		if (len > 0 && line[len - 1] == '\n')
-		{
 			line[len - 1] = '\0';
-		}
+
 		args = malloc(sizeof(char *) * (len + 1));
 		parse_input(line, args, &num);
 		if (_strncmp(line, "exit", 5) == 0)
-		{
 			exitt(line, args);
-		}
 		execute_command(args, argv, numm);
 		free(args);
 		args = NULL;
