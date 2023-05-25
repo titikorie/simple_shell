ba526f1ad48a615c299ac06a92b384364dbd0bbb
amel83 <tesfaneshk222@gmail.com>
2023-05-24 19:08:29 -0700

    c

--- free.c
+++ free.c
@@ -2,6 +2,9 @@
 /**
  * freee - to free allocated memories
  * @line: one of the pointers
+ * @args: pointer
+ * @path_copy: pointer
+ * @full_path: pointer
  * Return: nothin
  */
 void freee(char *line, char **args, char *path_copy, char *full_path)
