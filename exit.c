6b16552014dd75e556fbfdeec102541801d9cce3
amel83 <tesfaneshk222@gmail.com>
2023-05-25 10:50:35 -0700

    c

--- exit.c
+++ exit.c
@@ -2,20 +2,37 @@
 /**
  * exitt - to exit the child process
  * @line: read input command
+ * @argv: name of program
+ * @numm: counter
  * @args: points the argumentss
  */
-void exitt(char *line, char **args)
+void exitt(char *line, char **args, char **argv, int numm)
 {
 	int status = 0;
+	list_t list;
 
-	if (args == NULL || line == NULL)
+	if (_strncmp(args[0], "exit", 4) == 0)
 	{
-		free(line);
-		free(args);
-		exit(0);
+		if (args[1] == NULL)
+		{
+			free(line);
+			free(args);
+			exit(status);
+		}
+		status = _atoi(args[1]);
+		if (status >= 0)
+		{
+			free(args);
+			free(line);
+			exit(status);
+		}
+		else
+		{
+			list.args = args;
+			list.number = numm++;
+			_error(&list, ": Illegal number\n", argv, numm++);
+			status = 2;
+			exit(status);
+		}
 	}
-	status = _atoi(&line[5]);
-	free(args);
-	free(line);
-	exit(status);
 }
